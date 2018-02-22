f3f7bccc82a5c09e16f089b996d7233d5ed2a543
tangfansuperman <tangfan2015@163.com>
2018-02-22 11:08:52 +0800

    Initial commit

---
+++ app/src/main/java/com/exam/tangfan/testrecyclerview/FruitAdapter.java
@@ -0,0 +1,60 @@
+package com.exam.tangfan.testrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder> {
+
+    private List<Fruit> mFruitList;
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
+        final MyViewHolder holder = new MyViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(view.getContext(),"you clicked view "+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+    static class  MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
+        View fruitView;
+        public MyViewHolder(View view) {
+            super(view);
+            fruitView = view;
+            fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            fruitName = (TextView)view.findViewById(R.id.fruit_name);
+        }
+    }
+
+}
